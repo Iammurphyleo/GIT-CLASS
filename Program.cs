47995991c10a74e62a5c013b3f79ba1190c34626
Iammurphyleo <dadamuftau@gmail.com>
2021-11-15 11:59:48 +0100

    created a new C# program and adding of new line of code

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace gitclass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Write(" Dada Muftau");
+        
+        }
+    }
+}
